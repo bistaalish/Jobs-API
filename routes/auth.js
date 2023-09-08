8686e4ac53d5b216ca478852ecd8b5108f28ad93
Alish Bista <bistace321@gmail.com>
2023-09-08 16:18:29 +0545

    feat: created routes for jobs

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
+/* 
+This is Authentication Router.
+This router handles login and Authentication Routes for this program.
+*/
+
 const express = require('express');
 const router = express.Router()
 
 const {login,register} = require("../controllers/auth")
 
+// Handles the registration 
 router.post('/register',register)
+// Handles the login
 router.post("/login",login)
 
 module.exports = router
---
+++ routes/jobs.js
@@ -0,0 +1,11 @@
+/*
+This is CRUD routes for the Jobs API.s
+*/
+const express = require('express');
+const router = express.Router()
+const {getAllJobs,getJob,createJob,updateJob,deleteJob} = require("../controllers/jobs")
+
+router.route('/').get(getAllJobs).post(createJob)
+router.route('/:id').patch(updateJob).delete(deleteJob).get(getJob)
+
+module.exports = router
