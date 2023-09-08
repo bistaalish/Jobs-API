58f868cd5d81b28dd81252e8d276c6aa0d346f82
Alish Bista <bistace321@gmail.com>
2023-09-08 20:45:04 +0545

    feat: getJob method completed

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,14 +2,22 @@ const Job = require('../models/Job');
 const {StatusCodes} = require('http-status-codes');
 
 // Controller for get All jobs
-const getAllJobs = async (req, res) => {
-  //  res.send("Get all Jobs") 
+const getAllJobs = async (req, res) => { 
   const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
   res.status(StatusCodes.OK).json({jobs, count: jobs.length})
 }
 
-const getJob = (req,res) => {
-  res.send("Get Job")  
+const getJob = async (req,res) => {
+  // res.send("Get Job")  
+  const {user:{userId},params:{id: jobId}} = req
+  
+  const job = await Job.findOne({
+    _id:jobId,createdBy:userId
+  })
+  if(!job){
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json(job)
 }
 
 const createJob = async (req,res) => {
