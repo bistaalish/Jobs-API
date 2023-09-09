7f0b74dbf963610c4b0bc917512d60a26bc0e824
Alish Bista <bistace321@gmail.com>
2023-09-09 18:36:21 +0545

    feat: completed security

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const authRouter = require('./routes/auth');
 const jobRouter = require('./routes/jobs')
 const authMiddleware  = require('./middleware/authentication');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit')
+
 // ConnectDb
 const connectDB = require('./db/connect');
 const app = express();
@@ -12,8 +19,17 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 // routes
 app.get('/', (req, res) => {
